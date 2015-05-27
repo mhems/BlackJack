1ad4e619d5e693342433ecf86e031ba60b2ae63e
Matt Hems <mhems1@binghamton.edu>
2015-05-27 00:07:09 -0400

    initial basic logic

--- Blackjack/Basic/Card.py
+++ Blackjack/Basic/Card.py
@@ -5,4 +5,38 @@
 ####################
 
 class Card:
-    pass
+
+
+    ranks = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+    suits = ['S','H','D','C']
+
+    __charToNameDict = {}
+    for i in range(2,11):
+        __charToNameDict[i] = i
+    __charToNameDict['J'] = 'Jack'
+    __charToNameDict['Q'] = 'Queen'
+    __charToNameDict['K'] = 'King'
+    __charToNameDict['A'] = 'Ace'
+    __charToNameDict['S'] = 'Spades'
+    __charToNameDict['H'] = 'Hearts'
+    __charToNameDict['D'] = 'Diamonds'
+    __charToNameDict['C'] = 'Clubs'
+    
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        if self.rank not in Card.ranks:
+            raise TypeError("Rank must be a number 2-10 or J, Q, K, A (Jack, Queen, King, or Ace)")
+        if self.suit not in Card.suits:
+            raise TypeError("Suit must be one of S, H, D, C (Spades, Hearts, Diamonds, Clubs)")
+        
+    def __str__(self):
+        return str(Card.__charToNameDict[self.rank]) + ' of ' + Card.__charToNameDict[self.suit]
+
+    def __repr__(self):
+        return self.rank + ' of ' + self.suit
+
+if __name__ == '__main__':
+    c = Card(4,'D')
+    print(c)
+    #d = Card(4,'E')
--- Blackjack/Basic/Shoe.py
+++ Blackjack/Basic/Shoe.py
@@ -4,5 +4,51 @@
 #
 ####################
 
+from Deck import *
+
 class Shoe:
-    pass
+
+    def __init__(self, algorithm, n=6, end=7/8):
+        self.decks = []
+        self.numDecks = n
+        self.index = 0
+        self.algorithm = algorithm
+        self.end = n * 52 * end
+        for i in range(n):
+            self.decks.append(Deck.makeDeck())
+
+    def deal(self,n=1):
+        t = []
+        for i in range(n):
+            t.append(self.__dealOneCard())
+        return t
+
+    def __dealOneCard(self):
+        if self.isEmpty():
+            self.shuffle()
+        c = self.cards[self.index]
+        self.index += 1
+        return c
+    
+    def numCardsRemaining(self):
+        return self.numDecks * 52 - self.index
+    
+    def isEmpty(self):
+        return self.index >= self.numDecks * 52
+
+    def reset(self):
+        self.index = 0
+
+    def shuffle(self):
+        self.reset()
+        algorithm.shuffle(self.cards)
+
+    def __repr__(self):
+        return "%d out of %d" % (self.index, self.numDecks)
+
+    def __str__(self):
+        return "On card # %d out of %d (%d decks)" % (self.index, self.numDecks*52, self.numDecks)
+
+if __name__ == '__main__':
+    s = Shoe(algorithm=None)
+    print(s)
