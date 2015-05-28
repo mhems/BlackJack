9a7d0c505b250f4fb4acaf3c3b280377142f0075
Matt Hems <mhems1@binghamton.edu>
2015-05-28 00:43:53 -0400

    initial tests

---
+++ tests/Basic/testCard.py
@@ -0,0 +1,20 @@
+####################
+#
+# testCard.py
+#
+####################
+
+import unittest
+
+class testCard(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
