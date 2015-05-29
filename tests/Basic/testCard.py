8c2ca398fd9496ec337f95958594bf6de298672f
Matt Hems <mhems1@binghamton.edu>
2015-05-28 22:44:58 -0400

    test cases for Card.py

--- tests/Basic/testCard.py
+++ tests/Basic/testCard.py
@@ -4,17 +4,89 @@
 #
 ####################
 
+
+from src.Basic.Card import *
+from itertools import *
 import unittest
 
 class testCard(unittest.TestCase):
+
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test(self):
-        pass
+    def testInit(self):
+        c = Card(4,'H')
+        self.assertEqual(c.rank,4,"testCard:testInit:Card rank not initialized properly")
+        self.assertEqual(c.suit,'Hearts',"testCard:testInit:Card suit not initialized properly")
+        self.assertRaises(TypeError,Card.__init__,'B','D')
+        self.assertRaises(TypeError,Card.__init__,'K','B')
+        c1 = Card(4,'hearts')
+        c2 = Card(4,'h')
+        c3 = Card(4,'Hearts')
+        
+    def testRank(self):
+        c = Card('J','H')
+        self.assertEqual(c.rank,'Jack','testCard:testRank:Card rank doesn\'t evaluate properly')
 
+    def testSuit(self):
+        c = Card(10,'H')
+        self.assertEqual(c.suit,'Hearts','testCard:testRank:Card suit doesn\'t evaluate properly')
+        
+    def testValue(self):
+        num = Card(8,'S')
+        self.assertEqual(num.value,8,'testCard:testValue:Numeric card value doesn\'t evaluate properly')
+        face = Card('K','S')
+        self.assertEqual(face.value, 10,'testCard:testValue:Face card value doesn\'t evaluate properly')
+        ace = Card('A','S')
+        self.assertEqual(ace.value, Card.HARD_ACE_VALUE,'testCard:testValue:Ace value doesn\'t evaluate properly')
+
+    def testStr(self):
+        c = Card('A','S')
+        self.assertEqual(str(c),'Ace of Spades','testCard:testStr: str not working')
+
+    def testRepr(self):
+        c = Card('A','S')
+        self.assertEqual(repr(c),'Ace of Spades','testCard:testStr: repr not working')
+
+    def testEQ(self):
+        c = Card(4,'H')
+        c1 = Card(4,'hearts')
+        c2 = Card(4,'h')
+        c3 = Card(4,'Hearts')
+        self.assertEqual(c,c1,'testCard:testEq:Cards should be equal')
+        self.assertEqual(c1,c2,'testCard:testEq:Cards should be equal')
+        self.assertEqual(c2,c3,'testCard:testEq:Cards should be equal')
+        a = Card('A','S')
+        self.assertNotEqual(c,a,'testCard:testEq:Cards should not be equal')
+
+    def testLE(self):
+        c = Card(5,'H')
+        n = Card(9,'S')
+        self.assertTrue(c <  n,'testCard:testLE:Cards expected to be in order')
+        self.assertFalse(n < c,'testCard:testLE:Cards expected to not be in order')
+
+    def testIsAce(self):
+        c = Card('A','H')
+        self.assertTrue(c.isAce(),'testCard:testIsAce:Ace should return True')
+        n = Card(9,'H')
+        self.assertFalse(n.isAce(),'testCard:testIsAce:Non-ace should return False')
+
+    def testIsFaceCard(self):
+        k = Card('K','C')
+        self.assertTrue(k.isFaceCard(),'testCard:testFaceCard:King should return True')
+        a = Card('A','C')
+        self.assertTrue(a.isFaceCard(),'testCard:testFaceCard:Ace should return True')
+        t = Card(10,'C')
+        self.assertFalse(t.isFaceCard(),'testCard:testFaceCard:Non-face card should return False')
+
+    def testMakeDeck(self):
+        d = Card.makeDeck()
+        self.assertEqual(len(d),52,'testCard:testMakeDeck:Deck should have 52 cards')
+        self.assertEqual(len(list(filter(lambda c:c.suit=='Hearts',d))),13,'testCard:testMakeDeck:Deck should have 13 cards of hearts suit')
+        self.assertEqual(len(list(filter(lambda c:c.rank=='Ace',d))),4,'testCard:testMakeDeck:Deck should have 4 aces')
+        
 if __name__ == '__main__':
     unittest.main()
